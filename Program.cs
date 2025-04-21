94a3ffa9dacc2fb4bd079eea24adf12f046d7d51
rmqrnjs6 <58199377+rmqrnjs6@users.noreply.github.com>
2025-04-21 17:02:31 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TRPG
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            // 게임 시작 화면
+
+
+            // 상태 정보
+
+
+            //
+        }
+    }
+
+}
